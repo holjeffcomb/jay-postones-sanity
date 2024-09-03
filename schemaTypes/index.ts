f1825c2ea416fdac303eb606c3cad334b0f52843
Jeff Holcomb <jeffh.84@gmail.com>
2024-09-03 14:56:00 -0700

    change order of documents

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,5 +1,5 @@
-import user from './user'
 import lesson from './lesson'
 import course from './course'
+import user from './user'
 
-export const schemaTypes = [user, lesson, course]
+export const schemaTypes = [lesson, course, user]
